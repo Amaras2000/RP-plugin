393a4f966a0497aa6fe5869bb0a7cca155c88af0
BuildTools <unconfigured@null.spigotmc.org>
2015-05-29 22:58:22 +0200

    first

---
+++ src/fr/amaras/rp/RP.java
@@ -0,0 +1,11 @@
+package fr.amaras.rp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RP extends JavaPlugin{
+	
+	public void onEnable(){
+		getLogger().info("[RP Plugin] is enable");
+	}
+
+}
